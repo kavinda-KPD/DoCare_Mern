76bab92a69b60939e9d946b73513a0a701a1a091
kavinda dissanayake <hadapangodage.dissanayaka@scicom.com.my>
2023-12-19 00:15:04 +0530

    complete registration

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Button, Flex } from 'antd';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-
+import { Toaster } from 'react-hot-toast';
 
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster position="top-center" reverseOrder={false} />
 
       <Routes>
 
-        <Route path='/login' element={<Login/>} />
-        <Route path='/Register' element={<Register/>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/Register' element={<Register />} />
 
       </Routes>
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import {Button, Form , Input} from 'antd'
+import { Link } from 'react-router-dom'
 
 function Login() {
+
+
+  const onFinish = (values) => {
+      console.log(values);
+  }
+
   return (
-    <div>Login</div>
+    <div className='authentication'>
+      <div className='authentication-form card p-3'>
+        <h1 className='card-title'>Welcome Back</h1>
+
+        <Form layout='vertical' onFinish={onFinish}>
+
+          <Form.Item label='Email' name='email'>
+            <Input placeholder='Email' />
+          </Form.Item>
+
+          <Form.Item label='Password' name='password'>
+            <Input placeholder='Password' type='password'  />
+          </Form.Item>
+
+          <Button className='primary-button my-2' htmlType='submit'>Login</Button>
+          <Link to='/register' className='anchor'>Click here to Register</Link>
+        </Form>
+
+      </div>
+    </div>
   )
 }
 
